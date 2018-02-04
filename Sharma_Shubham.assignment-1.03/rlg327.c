337c0915da5e6d49428ebdd7c07c9085b6099a26
Shubham Sharma <shubham@iastate.edu>
2018-02-04 17:32:52 -0600

    Done with spawning PC and now working on path finding algorithm

--- Sharma_Shubham.assignment-1.03/rlg327.c
+++ Sharma_Shubham.assignment-1.03/rlg327.c
@@ -1087,6 +1087,177 @@ void usage(char *name)
   exit(-1);
 }
 
+// Picks a number between the bounds with the bounds included in the range
+int pickAnumber(int min, int max){
+
+	// This function was inspired from a code on StackOverflow
+	int r;
+    int range = 1 + max - min;
+	int buckets = RAND_MAX / range;
+	int limit = buckets * range;
+    do
+    {
+        r = rand();
+    } while (r >= limit);
+
+    return min + (r / buckets);
+}
+
+static void find_path_using_dijkstra(dungeon_t *d, pair_t from, pair_t to)
+{
+  static corridor_path_t path[DUNGEON_Y][DUNGEON_X], *p;
+  static uint32_t initialized = 0;
+  heap_t h;
+  uint32_t x, y;
+
+  if (!initialized) {
+    for (y = 0; y < DUNGEON_Y; y++) {
+      for (x = 0; x < DUNGEON_X; x++) {
+        path[y][x].pos[dim_y] = y;
+        path[y][x].pos[dim_x] = x;
+      }
+    }
+    initialized = 1;
+  }
+  
+  for (y = 0; y < DUNGEON_Y; y++) {
+    for (x = 0; x < DUNGEON_X; x++) {
+      path[y][x].cost = INT_MAX;
+    }
+  }
+
+  path[from[dim_y]][from[dim_x]].cost = 0;
+
+  heap_init(&h, corridor_path_cmp, NULL);
+
+  for (y = 0; y < DUNGEON_Y; y++) {
+    for (x = 0; x < DUNGEON_X; x++) {
+      if (mapxy(x, y) != ter_wall_immutable) {
+        path[y][x].hn = heap_insert(&h, &path[y][x]);
+      } else {
+        path[y][x].hn = NULL;
+      }
+    }
+  }
+
+  while ((p = heap_remove_min(&h))) {
+    p->hn = NULL;
+
+    if ((p->pos[dim_y] == to[dim_y]) && p->pos[dim_x] == to[dim_x]) {
+      for (x = to[dim_x], y = to[dim_y];
+           (x != from[dim_x]) || (y != from[dim_y]);
+           p = &path[y][x], x = p->from[dim_x], y = p->from[dim_y]) {
+        if (mapxy(x, y) != ter_floor_room) {
+          mapxy(x, y) = ter_floor_hall;
+          hardnessxy(x, y) = 0;
+        }
+      }
+      heap_delete(&h);
+      return;
+    }
+
+    if ((path[p->pos[dim_y] - 1][p->pos[dim_x]    ].hn) &&
+        (path[p->pos[dim_y] - 1][p->pos[dim_x]    ].cost >
+         p->cost + hardnesspair(p->pos))) {
+      path[p->pos[dim_y] - 1][p->pos[dim_x]    ].cost =
+        p->cost + hardnesspair(p->pos);
+      path[p->pos[dim_y] - 1][p->pos[dim_x]    ].from[dim_y] = p->pos[dim_y];
+      path[p->pos[dim_y] - 1][p->pos[dim_x]    ].from[dim_x] = p->pos[dim_x];
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] - 1]
+                                           [p->pos[dim_x]    ].hn);
+    }
+    if ((path[p->pos[dim_y]    ][p->pos[dim_x] - 1].hn) &&
+        (path[p->pos[dim_y]    ][p->pos[dim_x] - 1].cost >
+         p->cost + hardnesspair(p->pos))) {
+      path[p->pos[dim_y]    ][p->pos[dim_x] - 1].cost =
+        p->cost + hardnesspair(p->pos);
+      path[p->pos[dim_y]    ][p->pos[dim_x] - 1].from[dim_y] = p->pos[dim_y];
+      path[p->pos[dim_y]    ][p->pos[dim_x] - 1].from[dim_x] = p->pos[dim_x];
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y]    ]
+                                           [p->pos[dim_x] - 1].hn);
+    }
+    if ((path[p->pos[dim_y]    ][p->pos[dim_x] + 1].hn) &&
+        (path[p->pos[dim_y]    ][p->pos[dim_x] + 1].cost >
+         p->cost + hardnesspair(p->pos))) {
+      path[p->pos[dim_y]    ][p->pos[dim_x] + 1].cost =
+        p->cost + hardnesspair(p->pos);
+      path[p->pos[dim_y]    ][p->pos[dim_x] + 1].from[dim_y] = p->pos[dim_y];
+      path[p->pos[dim_y]    ][p->pos[dim_x] + 1].from[dim_x] = p->pos[dim_x];
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y]    ]
+                                           [p->pos[dim_x] + 1].hn);
+    }
+    if ((path[p->pos[dim_y] + 1][p->pos[dim_x]    ].hn) &&
+        (path[p->pos[dim_y] + 1][p->pos[dim_x]    ].cost >
+         p->cost + hardnesspair(p->pos))) {
+      path[p->pos[dim_y] + 1][p->pos[dim_x]    ].cost =
+        p->cost + hardnesspair(p->pos);
+      path[p->pos[dim_y] + 1][p->pos[dim_x]    ].from[dim_y] = p->pos[dim_y];
+      path[p->pos[dim_y] + 1][p->pos[dim_x]    ].from[dim_x] = p->pos[dim_x];
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] + 1]
+                                           [p->pos[dim_x]    ].hn);
+    }
+  }
+}
+
+void path_finding(dungeon_t *d)
+{
+	printf("PathFinding\n");
+	
+	pair_t from;
+	pair_t to;
+	pair_t p;
+
+	int yCor =0;
+	int xCor =0;
+	
+	// Picks a place for the PC to spawn
+	while(d->map[yCor][xCor] != ter_floor_room && d->map[yCor][xCor] != ter_floor_hall){
+		xCor = pickAnumber(0,DUNGEON_X-1);
+		yCor = pickAnumber(0,DUNGEON_Y-1);
+	}
+		
+	for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+		for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+		
+			find_path_using_dijkstra(&d, )
+		
+		}
+	}
+	
+	
+	//printf("Picked:(%d,%d)\n",xCor,yCor);
+	
+	for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+		for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+			
+			if(p[dim_x] == xCor && p[dim_y] == yCor) // Place the PC
+				putchar('@');
+			else
+			{
+				switch (mappair(p)) {
+				case ter_wall:
+				case ter_wall_immutable:
+					putchar(' ');
+					break;
+				case ter_floor:
+				case ter_floor_room:
+					putchar('.');
+					break;
+				case ter_floor_hall:
+					putchar('.');
+					break;
+				case ter_debug:
+					putchar('*');
+					fprintf(stderr, "Debug character at %d, %d\n", p[dim_y], p[dim_x]);
+					break;
+				}
+			}
+		}	
+		putchar('\n');
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
   dungeon_t d;
@@ -1238,6 +1409,8 @@ int main(int argc, char *argv[])
       free(save_file);
     }
   }
+  
+  path_finding(&d);
 
   delete_dungeon(&d);
 
