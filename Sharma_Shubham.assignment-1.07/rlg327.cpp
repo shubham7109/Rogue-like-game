9ce57c4e5beffb331770954a5ee0b99620717f54
Shubham Sharma <shubham@iastate.edu>
2018-03-27 23:48:24 -0500

    Starting 1.07

---
+++ Sharma_Shubham.assignment-1.07/rlg327.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <sys/time.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+  std::cout << "/* message */" << '\n';
+
+  return 0;
+}
