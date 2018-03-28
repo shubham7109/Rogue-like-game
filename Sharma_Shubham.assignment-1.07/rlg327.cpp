8b19503206a78a6648dc65d4e84dfd7a6112e09f
Shubham Sharma <shubham@iastate.edu>
2018-03-28 04:05:14 -0500

    Finishing up proj

--- Sharma_Shubham.assignment-1.07/rlg327.cpp
+++ Sharma_Shubham.assignment-1.07/rlg327.cpp
@@ -10,30 +10,77 @@ using namespace std;
 class MonsterDefinitions
 {
 
-    public:
-    string NAME;
-    string DESC;
-    string COLOR;
-    string SPEED;
-    string ABIL;
-    string HP;
-    string DAM;
-    string RRTY;
-    bool begin = false;
-    bool end = false;
+    public: // Everything is set to public
+      // File params
+    string name;
+    string description;
+    string symb;
+    string color;
+    string speed;
+    string abilities;
+    string hitpoints;
+    string damage;
+    string rarity;
+
+    string line; // Reads
+    bool fillAll = true; // Checker to see if all fields are given
+    bool justEnded = false;
+
+
+    void printMonsterDef(){
+
+
+      cout << name << "\n";
+      cout << description;
+      cout << symb << "\n";
+      cout << color << "\n";
+      cout << speed << "\n";
+      cout << abilities << "\n";
+      cout << hitpoints << "\n";
+      cout << damage << "\n";
+      cout << rarity << "\n";
+
+
+
+
+
+      cout << '\n';
+      writeMonsterDef();
+      resetMonsters();
+    }
+
+    void writeMonsterDef(){
+
+      // This is optional
+
+/*      char *path = strcat(getenv("HOME"), "/.rlg327/object desc.txt");;
+      std::cout << path << '\n';
+      ofstream outfile (path);
+      if (outfile.is_open())
+      {
+        outfile << "Name: " << name << "\n";
+        outfile << "Rarity: " << rarity << "\n";
+        outfile << "Color: " << color << "\n";
+        outfile << "Speed: " << speed << "\n";
+        outfile << "Damage: " << damage << "\n";
+        outfile << "Hit points: " << hitpoints << "\n";
+        outfile << "Abillities: " << abilities << "\n";
+        outfile << "Description: " << description << "\n";
+        myfile.close();
+      }
+      else cout << "Unable to open file";
+*/
+    }
 
     // Constructor to set up instance variables
     MonsterDefinitions(){
-
       char *path = strcat(getenv("HOME"), "/.rlg327/monster_desc.txt");;
-      string metadata;
-      ifstream monster_desc (path);
-      if (monster_desc.is_open())
+      ifstream inFile (path);
+      if (inFile.is_open())
       {
-        metadata = "";
-        getline (monster_desc,metadata);
-        if(metadata != "RLG327 MONSTER DESCRIPTION 1"){
-          std::cout << metadata << "\n";
+        line = "";
+        getline (inFile,line);
+        if(line != "RLG327 MONSTER DESCRIPTION 1"){
           std::cout << "Line 1 is incorrect!" << "\n";
           exit(0); // exits the program
         }
@@ -42,134 +89,129 @@ class MonsterDefinitions
         std::cout << "File not found!" << "\n";
         exit(0);
       }
+      description = "";
 
-      string line;
-      int flag = 0;
-      while(getline(monster_desc, line)){
-        std::cout << line << "\n";
-        if(line == "" || line == "BEGIN MONSTER"){
-          continue;
+      // Reads till eof
+      while(getline(inFile, line)){
+        if(justEnded && line != "BEGIN MONSTER" && line != "" ){
+          std::cout << "check BEGIN MONSTER / END line" << '\n';
+          exit(0);
         }
-        if(line ==  "END"){
+        if(justEnded && line == "BEGIN MONSTER"){
+          justEnded = false;
+        }
 
-          if(NAME.length() == 0 ||
-          RRTY.length() == 0 ||
-          COLOR.length() == 0 ||
-    	    SPEED.length() == 0 ||
-          DAM.length() == 0 ||
-          HP.length() == 0 ||
-    	    ABIL.length() == 0 ||
-          DESC.length() == 0 ||
-          flag == 1){
-             flag = 0;
+        if(line ==  "END"){
+          justEnded = true;
+          if(fillAll == false){
+             fillAll = true;
           }
           else{
-    	//monster has everything, print to console
-    	cout << "Name: " << NAME << "\n";
-    	cout << "Rarity: " << RRTY << "\n";
-    	cout << "Color: " << COLOR << "\n";
-    	cout << "Speed: " << SPEED << "\n";
-    	cout << "Damage: " << DAM << "\n";
-    	cout << "Hit points: " << HP << "\n";
-    	cout << "Abillities: " << ABIL << "\n";
-    	cout << "Description: " << DESC << "\n";;
-    	cout << "\n" << "\n";
-    	NAME = RRTY = COLOR = SPEED = DAM = HP = ABIL = DESC = "";
+            printMonsterDef();
           }
         }
-        else if(line.substr(0, 4) == "NAME"){
-          if(NAME != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	       flag = 1;
+
+
+        if(line.substr(0, 4) == "NAME"){
+          if(name != ""){
+            //std::cout << "SETTING FLAG TO 1" << "\n";
+    	       fillAll = false;
           }
-          NAME = line.substr(5, line.length());
+          name = line.substr(5, line.length());
         }
-        else if(line.substr(0, 4) == "RRTY"){
-          if(RRTY != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          RRTY = line.substr(5, line.length());
-        }
-        else if(line.substr(0, 5) == "COLOR"){
-          if(COLOR != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          COLOR = line.substr(5, line.length());
-        }
-        else if(line.substr(0, 5) == "SPEED"){
-          if(SPEED != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          SPEED = line.substr(6, line.length());
-        }
-        else if(line.substr(0, 3) == "DAM"){
-          if(DAM != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          DAM = line.substr(4, line.length());
-        }
-        else if(line.substr(0, 2) == "HP"){
-          if(HP != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          HP = line.substr(3, line.length());
-        }
-        else if(line.substr(0, 4) == "ABIL"){
-          if(ABIL != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          ABIL = line.substr(5, line.length());
-        }
-        else if(line.substr(0, 4) == "DESC"){
-          if(DESC != ""){
-            std::cout << "SETTING FLAG TO 1" << "\n";
-    	flag = 1;
-          }
-          while(getline(monster_desc, line)){
-    	if(line == "."){
-    	  break;
-    	}
-    	if(line == "DESC"){
-    	  continue;
-    	}
-    	else if(DESC == ""){
-    	  if(line.length() > 77){
-          std::cout << "SETTING FLAG TO 1" << "\n";
-    	    flag = 1;
-    	  }
-    	  DESC = line + "\n";
-    	}
-    	else{
-    	  if(line.length() > 77){
-          std::cout << "SETTING FLAG TO 1" << "\n";
-    	    flag = 1;
-    	  }
-    	  DESC = DESC + line + "\n";
-    	}
+
+
+        if(line.substr(0, 4) == "DESC"){
+          if(description != ""){
+            //std::cout << "SETTING FLAG TO 1" << "\n";
+            fillAll = false;
+          }// Gets all the description lines
+          while(getline(inFile, line)){
+            if(line == "."){
+              break;
+            }
+            if(line == "DESC"){
+              continue;
+            }
+            else{
+              if(line.length() > 77){
+                //std::cout << "SETTING FLAG TO 1" << "\n";
+                fillAll = false;
+              }
+              description = description + line + "\n";
+            }
           }
         }
-        else{
-            continue;
-        }
-      }
-      monster_desc.close();
+
+
+        if(line.substr(0, 4) == "SYMB"){
+          if(symb != ""){
+            //std::cout << "SETTING FLAG TO 1" << "\n";
+             fillAll = false;
+           }
+           symb = line.substr(5, line.length());
+         }
+
+
+        if(line.substr(0, 5) == "COLOR"){
+          if(color != ""){
+            //std::cout << "SETTING FLAG TO 1" << "\n";
+             fillAll = false;
+           }
+           color = line.substr(6, line.length());
+         }
+
+
+         if(line.substr(0, 5) == "SPEED"){
+           if(speed != ""){
+             //std::cout << "SETTING FLAG TO 1" << "\n";
+             fillAll = false;
+            }
+            speed = line.substr(6, line.length());
+          }
+
+
+         if(line.substr(0, 4) == "ABIL"){
+           if(abilities != ""){
+             //std::cout << "SETTING FLAG TO 1" << "\n";
+             fillAll = false;
+            }
+            abilities = line.substr(5, line.length());
+          }
+
+
+          if(line.substr(0, 2) == "HP"){
+            if(hitpoints != ""){
+              //::cout << "SETTING FLAG TO 1" << "\n";
+               fillAll = false;
+             }
+             hitpoints = line.substr(3, line.length());
+           }
+
+
+         if(line.substr(0, 3) == "DAM"){
+           if(damage != ""){
+             //std::cout << "SETTING FLAG TO 1" << "\n";
+	            fillAll = false;
+            }
+            damage = line.substr(4, line.length());
+          }
+
+
+          if(line.substr(0, 4) == "RRTY"){
+          if(rarity != ""){
+            //  std::cout << "SETTING FLAG TO 1" << "\n";
+  	         fillAll = false;
+           }
+           rarity = line.substr(5, line.length());
+         }
+       }
+
+      inFile.close();
 
     }
-
-    // Member Functions()
-    void printMonster()
-    {
-       std::cout << "Print monster" << std::endl;;
-    }
-
-    void resetMonster(){
-      std::cout << "Reset monster" << std::endl;
+    void resetMonsters(){
+      name = rarity = color = symb = speed = damage = hitpoints = abilities = description = "";
     }
 };
 
@@ -179,6 +221,8 @@ int main(int argc, char *argv[])
 
   // Calls Constructor
   MonsterDefinitions monsterObj;
+  // This should basically handle eveything
+
 
   return 0;
 }
